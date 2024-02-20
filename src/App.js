a514a328eaead41adb1e37e2a9113de200cdef40
GamjaJEON <indian_potato@naver.com>
2024-02-20 17:14:43 +0900

    feat: delete버튼 id 받아오기

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import { Component } from 'react'
 import './App.css'
+import ExpenseForm from './components/ExpenseForm'
+import ExpenseList from './components/ExpenseList'
 
 export default class App extends Component {
+  initialExpense = [
+    { id: 1, charge: '콜라', amount: 2000 },
+    { id: 2, charge: '빵', amount: 1000 },
+    { id: 3, charge: '맥북', amount: 2000000 },
+  ]
+
+  handleDelete = (id) => {
+    const newExpense = this.initialExpense.filter((expense) => expense.id !== id)
+    console.log(newExpense)
+  }
+
   render() {
     return (
       <div className="App">
@@ -9,10 +22,13 @@ export default class App extends Component {
           <div className="sub-container">
             <h1>장바구니</h1>
             <div style={{ width: '100%', backgroundColor: 'white', padding: '1rem' }}>
-              {}
+              <ExpenseForm />
             </div>
             <div style={{ width: '100%', backgroundColor: 'white', padding: '1rem' }}>
-              {}
+              <ExpenseList
+                initialExpense={this.initialExpense}
+                handleDelete={this.handleDelete}
+              />
             </div>
             <div style={{ display: 'flex', justifyContent: 'start', marginTop: '1rem' }}>
               <p style={{ fontSize: '2rem' }}>총합계:</p>
